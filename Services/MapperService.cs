7fe4608ae1cab6b5e23dd7a45c906894f440b021
rinatraev <64296176+rinatraev@users.noreply.github.com>
2025-09-11 19:00:56 +0500

    MapperService class created

---
+++ Services/MapperService.cs
@@ -0,0 +1,28 @@
+﻿using MedicalAPI.Models;
+using MedicalAPI.Entities;
+namespace MedicalAPI.Services;
+
+public static class MapperService
+{
+	public static VisitToClientResponseDto MapVisitToClientResponseDto(Visit visit)
+	{
+		return new VisitToClientResponseDto
+		{
+			Id = visit.Id,
+			Doctor = visit.Doctor != null ? new DoctorToClientDto
+			{
+				Id = visit.Doctor.Id,
+				LastName = visit.Doctor.LastName,
+				FirstName = visit.Doctor.FirstName,
+				Patronymic = visit.Doctor.Patronymic,
+				Specialization = visit.Doctor.Specialization,
+				Position = visit.Doctor.Position,
+				ContactNumber = visit.Doctor.ContactNumber
+			} : null,
+			Status = visit.Status,
+			Date = visit.Date,
+			CreatedAt = visit.CreatedAt,
+			UpdatedAt = visit.UpdatedAt
+		};
+	}
+}
